5c8465c675521b31df630fc5059100b5c4833871
Jordan Hand <jordanhand@comcast.net>
2016-05-06 11:17:50 -0700

    added comments

--- exercise-3/js/Calc.js
+++ exercise-3/js/Calc.js
@@ -1,8 +1,11 @@
 window.Calc = (function() {
+    // Write a constructor function which takes a number, stores it, and
+    // defines all Calc API functions
     var c = function(num) {
         var calc = {};
         calc.currNum = num;
 
+        // Implement getVal, plus, minus, times, divide, and clear
         calc.getVal = function() {
             return this.currNum;
         }
@@ -35,5 +38,6 @@ window.Calc = (function() {
         return calc;
     };
 
+    // Return your constructed object
     return c;
 })();
